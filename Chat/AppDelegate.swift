fe1415ab41a4bb2ee484ff51ca9a5c228facfa8e
Maria Myamlina <mariamyamlina@gmail.com>
2020-09-12 00:47:14 +0300

    final change

--- Chat/AppDelegate.swift
+++ Chat/AppDelegate.swift
@@ -17,48 +17,48 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     //Индикатор наличия логов приложения
     static let appLogIndicator = true
     
-    let notRun = "not running state"
-    let fore = "foreground"
-    let inAct = "inactive state"
-    let act = "active state"
-    let back = "background"
-    let susp = "suspended state"
+    let notRun = "'not running state'"
+    let fore = "'foreground'"
+    let inAct = "'inactive state'"
+    let act = "'active state'"
+    let back = "'background'"
+    let susp = "'suspended state'"
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         if AppDelegate.appLogIndicator {
-            print("Application moved from \(notRun) to \(inAct): \(#function)")
+            print("Application moved from \(notRun) to \(inAct): \n" + "called \(#function)\n")
         }
         return true
     }
     
     func applicationDidBecomeActive(_ application: UIApplication) {
         if AppDelegate.appLogIndicator {
-            print("Application moved from \(inAct) to \(act): \(#function)")
+            print("Application moved from \(inAct) to \(act): \n" + "called \(#function)\n")
         }
     }
     
     func applicationWillResignActive(_ application: UIApplication) {
         if AppDelegate.appLogIndicator {
-            print("Application moved from \(act) to \(inAct): \(#function)")
+            print("Application moved from \(act) to \(inAct): \n" + "called \(#function)\n")
         }
     }
     
     func applicationWillEnterForeground(_ application: UIApplication) {
         if AppDelegate.appLogIndicator {
-            print("Application moved from \(back) to \(inAct): \(#function)")
+            print("Application moved from \(back) to \(inAct): \n" + "called \(#function)\n")
         }
     }
     
     func applicationDidEnterBackground(_ application: UIApplication) {
         if AppDelegate.appLogIndicator {
-            print("Application moved from \(inAct) to \(back): \(#function)")
+            print("Application moved from \(inAct) to \(back): \n" + "called \(#function)\n")
         }
     }
     
     func applicationWillTerminate(_ application: UIApplication) {
         if AppDelegate.appLogIndicator {
-            print("Application is about to terminate: \(#function)")
+            print("Application is about to terminate: \n" + "called \(#function)\n")
         }
     }
 
--- Chat/ViewController.swift
+++ Chat/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let appIng = "'appearing'"
+    let appEd = "'appeared'"
+    let disappIng = "'disappearing'"
+    let disappEd = "'disappeared'"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,42 +23,42 @@ class ViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         if AppDelegate.appLogIndicator {
-            print("ViewController moved from disappeared to appearing: \(#function)")
+            print("ViewController moved from \(disappEd) to \(appIng): \n" + "called \(#function)\n")
         }
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         if AppDelegate.appLogIndicator {
-            print("ViewController moved from appearing to appeared: \(#function)")
+            print("ViewController moved from \(appIng) to \(appEd): \n" + "called \(#function)\n")
         }
     }
 
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
         if AppDelegate.appLogIndicator {
-            print("ViewController's view is about to layout its subviews: \(#function)")
+            print("ViewController's view is about to layout its subviews: \n" + "called \(#function)\n")
         }
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         if AppDelegate.appLogIndicator {
-            print("ViewController's view has just laid out its subviews: \(#function)")
+            print("ViewController's view has just laid out its subviews: \n" + "called \(#function)\n")
         }
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         if AppDelegate.appLogIndicator {
-            print("ViewController moved from appearing/appeared to disappearing: \(#function)")
+            print("ViewController moved from \(appIng)/\(appEd) to \(disappIng): \n" + "called \(#function)\n")
         }
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidAppear(animated)
         if AppDelegate.appLogIndicator {
-            print("ViewController moved from disappearing to disappeared/appearing: \(#function)")
+            print("ViewController moved from \(disappIng) to \(disappEd)/\(appIng): \n" + "called \(#function)\n")
         }
     }
 }
